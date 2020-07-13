d319c690db229c47a8749e79945825a3868617e2
ove <mrvangogh@yeah.net>
2020-07-13 16:16:11 +0800

    第一次上传代码

---
+++ src/main/java/com/etoak/Springboot001GitApplication.java
@@ -0,0 +1,14 @@
+package com.etoak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot001GitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(
+                Springboot001GitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/etoak/Springboot001GitApplicationTests.java
@@ -0,0 +1,29 @@
+package com.etoak;
+
+import com.etoak.entity.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
+import java.util.Optional;
+
+@SpringBootTest
+class Springboot001GitApplicationTests {
+
+    @Test
+    void contextLoads() {
+        User user = new User();
+        user.setId(1);user.setUserName("张三");
+        user.setCreateTime(new Date());
+        user.setPassword("1234");
+
+        Optional<User> optionalUser = Optional.of(user);
+        /*optionalUser.ifPresent(new Consumer<User>() {
+            @Override
+            public void accept(User user) {
+
+            }
+        });*/
+    }
+
+}
